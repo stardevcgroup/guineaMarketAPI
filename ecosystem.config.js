de893aab538ab8d051963c18516a13d45ee007d5
Laby Damaro Camara <ldamaro98@gmail.com>
2021-04-29 03:55:45 +0200

    ecosystempm2 deployed

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'bin/www',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'vps-6d770681.vps.ovh.net',
+      host : '152.228.217.119',
+      ref  : 'origin/main',
+      repo : 'https://github.com/stardevcgroup/guineaMarketAPI.git',
+      path : '/var/www/',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
