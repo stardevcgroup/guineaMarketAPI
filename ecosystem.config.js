22d67b2bd1949f17ed5894ae6701963884775498
Laby Damaro Camara <ldamaro98@gmail.com>
2021-04-29 04:02:30 +0200

    rec

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,14 +9,14 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'vps-6d770681.vps.ovh.net',
+      user : 'ubuntu',
       host : '152.228.217.119',
       ref  : 'origin/main',
       repo : 'https://github.com/stardevcgroup/guineaMarketAPI.git',
       path : '/var/www/',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': '',
     }
   }
 };
