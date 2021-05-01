08754b37c339da6d7a875fb76ae0e7cdbea54cc9
Laby Damaro Camara <ldamaro98@gmail.com>
2021-05-01 23:24:22 +0200

    Autre update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,8 +16,8 @@ module.exports = {
       host : '152.228.217.119',
       ref  : 'origin/main',
       repo : 'git@github.com:stardevcgroup/guineaMarketAPI.git',
-      path : '/var/www',
-      'pre-deploy-local': '',
+      path : 'C:\\Users\\damaro\\img\\guineaMarketAPI',
+      'pre-deploy-local': '/var/www',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
     }
